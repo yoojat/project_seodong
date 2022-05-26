ab33513f550f1cb60d286b9af129bb751da5974f
김태영 <kimtaeyoung@0Taes-Mac.local>
2022-05-26 13:16:36 +0900

    tailwind-styled-components 설치

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,21 @@
 import type { NextPage } from 'next';
+import tw from 'tailwind-styled-components';
+
+interface ButtonProps {
+  $primary: boolean;
+}
+
+const Button = tw.button<ButtonProps>`
+  flex
+  ${(props: ButtonProps) =>
+    props.$primary ? 'bg-indigo-600' : 'bg-indigo-300'}
+`;
 
 const Home: NextPage = () => {
   return (
     <div className='bg-red-500'>
       <h1 className='text-black'>it works</h1>
+      <Button $primary={true}>button</Button>
     </div>
   );
 };
