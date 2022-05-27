4a621753b6ffc4ac730ec1a5ab49acc01569cd36
김태영 <kimtaeyoung@0Taes-Mac.local>
2022-05-27 13:31:05 +0900

    seo 작업,메인 이미지 슬라이더 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,34 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { NextSeo } from 'next-seo';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <NextSeo
+        title='The Project Seodong'
+        description='지역의 필요를 담아 서동 공유공간을 만들고, 지역에 사는 사람들의 이야기를 담아 아카이빙하는 프로젝트 서동으로, 유휴공간재생 전문집단 크리플레이에서 주도하고 있다.'
+        canonical='https://project-seodong.vercel.app'
+        openGraph={{
+          url: 'https://project-seodong.vercel.app',
+          title: 'The Project Seodong',
+          description:
+            '지역의 필요를 담아 서동 공유공간을 만들고, 지역에 사는 사람들의 이야기를 담아 아카이빙하는 프로젝트 서동으로, 유휴공간재생 전문집단 크리플레이에서 주도하고 있다.',
+          images: [
+            {
+              url: 'https://project-seodong.vercel.app/img/1.jpg',
+              width: 800,
+              height: 600,
+              alt: 'The Project Seodong',
+              type: 'image/jpeg',
+            },
+          ],
+          site_name: 'The Project Seodong',
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next';
 import tw from 'tailwind-styled-components';
+import SimpleImageSlider from 'react-simple-image-slider';
 
 interface ButtonProps {
   $primary: boolean;
@@ -11,11 +12,35 @@ const Button = tw.button<ButtonProps>`
     props.$primary ? 'bg-indigo-600' : 'bg-indigo-300'}
 `;
 
+const images = [{ url: 'img/main/1.jpg' }, { url: 'img/main/4.jpg' }];
+
 const Home: NextPage = () => {
   return (
-    <div className='bg-red-500'>
-      <h1 className='text-black'>it works</h1>
-      <Button $primary={true}>button</Button>
+    <div className='h-screen'>
+      <div className='-z-10'>
+        <SimpleImageSlider
+          width={'100%'}
+          height={'100vh'}
+          images={images}
+          showBullets={true}
+          showNavs={false}
+          autoPlay={true}
+        />
+      </div>
+      <div
+        className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2
+ text-white text-center w-full'
+      >
+        <h1 className='text-3xl font-extralight select-none mt-20'>
+          The Project Seodong
+        </h1>
+        <button
+          onClick={() => alert('준비중입니다.')}
+          className='font-light text-sm border-[0.5px] border-slate-50/30 px-6 py-2 hover:bg-slate-200 hover:text-black transition-colors duration-300 mt-5 mb-10'
+        >
+          들어가기
+        </button>
+      </div>
     </div>
   );
 };
