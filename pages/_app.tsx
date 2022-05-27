647aacb53a6f0debbcecb8207780b22efda8274d
김태영 <kimtaeyoung@0Taes-Mac.local>
2022-05-27 13:33:38 +0900

    seo 이미지 url 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,7 +16,7 @@ function MyApp({ Component, pageProps }: AppProps) {
             '지역의 필요를 담아 서동 공유공간을 만들고, 지역에 사는 사람들의 이야기를 담아 아카이빙하는 프로젝트 서동으로, 유휴공간재생 전문집단 크리플레이에서 주도하고 있다.',
           images: [
             {
-              url: 'https://project-seodong.vercel.app/img/1.jpg',
+              url: 'https://project-seodong.vercel.app/img/main/1.jpg',
               width: 800,
               height: 600,
               alt: 'The Project Seodong',
