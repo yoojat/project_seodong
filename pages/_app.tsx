af61356e10edb131d5b484dcfb44219775d68dfb
김태영 <kimtaeyoung@0Taes-Mac.local>
2023-02-20 19:05:15 +0900

    키워드 변경

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,12 +7,12 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <NextSeo
-        title='서동부엌 서동영화 서동여관'
+        title='고개서동 프로젝트 - 서동부엌, 서동영화, 서동여관'
         description='골목길의 등불 조차 어색한 동네에 사람들의 이야기와 향기를 불어넣고 있습니다. 현재, 서동부엌, 서동영화, 서동여관 등을 운영하고 있습니다.'
         canonical='https://project-seodong.vercel.app'
         openGraph={{
           url: 'https://project-seodong.vercel.app',
-          title: '서동부엌 서동영화 서동여관',
+          title: '고개서동 프로젝트 - 서동부엌, 서동영화, 서동여관',
           description:
             '골목길의 등불 조차 어색한 동네에 사람들의 이야기와 향기를 불어넣고 있습니다. 현재, 서동부엌, 서동영화, 서동여관 등을 운영하고 있습니다.',
           images: [
